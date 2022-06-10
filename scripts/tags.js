22150aea18d56a49a299b3f31ee0801d6d5c7efc
亮玉 韦 <weily@yingmi.cn>
2022-06-11 00:27:18 +0800

    go instanceof + hexo tags

--- scripts/tags.js
+++ scripts/tags.js
@@ -2,18 +2,32 @@
 
 'use strict';
 
-hexo.extend.tag.register('note', (args, content) => {
-  const className = args.shift();
-  let header = '';
-  let result = '';
+hexo.extend.tag.register(
+  'note',
+  (args, content) => {
+    const className = args.shift();
+    let header = '';
+    let result = '';
 
-  if (args.length) {
-    header += `<strong class="note-title">${args.join(' ')}</strong>`;
-  }
+    if (args.length) {
+      header += `<strong class="note-title">${args.join(' ')}</strong>`;
+    }
 
-  result += `<blockquote class="note ${className}">${header}`;
-  result += hexo.render.renderSync({text: content, engine: 'markdown'});
-  result += '</blockquote>';
+    result += `<blockquote class="note ${className}">${header}`;
+    result += hexo.render.renderSync({ text: content, engine: 'markdown' });
+    result += '</blockquote>';
 
-  return result;
-}, true);
+    return result;
+  },
+  true
+);
+
+hexo.extend.tag.register('bilibili', ([bvid]) => {
+  const segments = [
+    '<div class="bilibili-container">TODO: 还有点问题',
+    `<iframe src="//player.bilibili.com/player.html?bvid=${bvid}"`,
+    ' scrolling="no" border="0" frameborder="no" width="800" height="600" allowfullscreen> </iframe>',
+    '</div>',
+  ];
+  return segments.join('');
+});
---
+++ source/nodejs/sites.md
@@ -0,0 +1,8 @@
+---
+title: Sites
+comments: false
+---
+
+* [npm rank](https://gist.github.com/anvaka/8e8fa57c7ee1350e3491)
+* [npm trends](https://www.npmtrends.com/): Compare package download counts over time
+* [node green](https://node.green/): 不同版本 node.js 对 js 语言特性支持情况的列表
