0fb58f7255bb2e01f6342f80acd1af2db7935188
亮玉 韦 <weily@yingmi.cn>
2022-07-19 00:53:53 +0800

    classic blog layout

--- src/design-patterns/cloud/template.js
+++ src/design-patterns/cloud/template.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouteData } from "react-static";
 import routeToData from "./routes";
-import Container from "../../components/Container";
+import ClassicBlogLayout from "../../components/ClassicBlogLayout";
 import TOC from "./toc";
 
 export default function CloudDesignPatternTemplate() {
@@ -9,18 +9,10 @@ export default function CloudDesignPatternTemplate() {
   const { Content, meta } = routeToData[path];
 
   return (
-    <div>
-      <div style={{ display: "flex" }}>
-        <div style={{ flexBasis: 300, flexShrink: 0 }}>
-          <h1>云设计模式</h1>
-          <TOC />
-        </div>
-        <div style={{ flexGrow: 1, maxWidth: 800 }}>
-          <h1>{meta.title}</h1>
-          <small>{path.substr(0, 10)}</small>
-          <Content />
-        </div>
-      </div>
-    </div>
+    <ClassicBlogLayout toc={<TOC />}>
+      <h1>{meta.title}</h1>
+      <small>{path.substr(0, 10)}</small>
+      <Content />
+    </ClassicBlogLayout>
   );
 }
