6651a7cebfe3aaca803a7b0e403908c38430557f
亮玉 韦 <weily@yingmi.cn>
2023-04-21 09:13:07 +0800

    levenstein

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), react()],
+  integrations: [mdx(), react()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
