2a443d2bea92c6dc05dbf201de083bd14e51d2f1
xyzwps <rocketeers@163.com>
2024-08-27 16:47:01 +0800

    tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), tailwind()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
+    rehypePlugins: [rehypeKatex]
+  }
 });
