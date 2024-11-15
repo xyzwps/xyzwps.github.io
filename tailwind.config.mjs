43ad8e3a4431a33da608dc3c285e4df30aaa4128
xyzwps <rocketeers@163.com>
2024-11-15 10:48:36 +0800

    spring cloud gateway

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      // see https://github.com/tailwindlabs/tailwindcss-typography/blob/main/src/styles.js
+      typography: {
+        DEFAULT: {
+          css: [
+            {
+              'code::before': { content: '', },
+              'code::after': { content: '', },
+              img: { width: 'unset' }
+            }
+          ]
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
