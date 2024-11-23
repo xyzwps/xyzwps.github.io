bde74f6230107ac27c8df5305554308220bd2b14
xyzwps <xyzwps@outlook.com>
2024-11-24 01:33:53 +0800

    cloud patterns

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,17 @@ import rehypeKatex from 'rehype-katex';
 
 import tailwind from "@astrojs/tailwind";
 
+import d2 from 'astro-d2';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), tailwind()],
+  integrations: [mdx(), react(), tailwind(), d2({
+    output: 'gen/d2',
+    theme: {
+      default: '6',
+      dark: false
+    }
+  })],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
