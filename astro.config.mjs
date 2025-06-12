77b7f8cc7009fdce67e596b02d840e3e7ff3b4fd
weily <weily@knnr.com>
2025-06-13 01:05:40 +0800

    初步升级到 tailwindcss 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,32 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwind from "@astrojs/tailwind";
-
-import d2 from 'astro-d2';
+import d2 from "astro-d2";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), tailwind(), d2({
-    output: 'gen/d2',
-    theme: {
-      default: '6',
-      dark: false
-    }
-  })],
+  integrations: [
+    mdx(),
+    react(),
+    d2({
+      output: "gen/d2",
+      theme: {
+        default: "6",
+        dark: false,
+      },
+    }),
+  ],
+
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
-  }
+    rehypePlugins: [rehypeKatex],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
