51c65dfd0e20721d20c50738a5ab0a9ef56efa7f
weily <weily@knnr.com>
2025-07-19 22:07:11 +0800

    styles

--- src/pages/app/json-prettier/JsonPrettier.tsx
+++ src/pages/app/json-prettier/JsonPrettier.tsx
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react";
-import Button from "../../../react/Button";
+import * as prettier from "prettier";
+import * as estreePlugin from "prettier/plugins/estree.js";
+import * as babelPlugin from "prettier/plugins/babel.js";
 
 const KEY = "___xyzwps_app_json_prettier__";
 
 export default function JsonPrettier() {
   const [json, setJson] = useState<string>("");
-  const prettier = () => {
-    setJson(JSON.stringify(JSON.parse(json), null, 2));
+  const formatJson = () => {
+    prettier
+
+      .format(json, {
+        parser: "json",
+        printWidth: 100,
+        objectWrap: "collapse",
+        // @ts-ignore
+        plugins: [estreePlugin, babelPlugin],
+      })
+      .then(setJson);
   };
 
   useEffect(() => {
@@ -20,16 +31,23 @@ export default function JsonPrettier() {
 
   return (
     <div className="p-2 gap-2 flex flex-col">
+      <div className="flex gap-2 justify-between items-center mx-4 my-2">
+        <h2 className="font-bold text-lg">JSON 格式化工具</h2>
+        <button
+          className="btn btn-primary btn-sm"
+          type="button"
+          onClick={formatJson}
+        >
+          格式化
+        </button>
+      </div>
       <textarea
         value={json}
         onChange={(e) => setJson(e.target.value)}
-        className="font-mono bg-white border border-indigo-100 rounded focus:outline-0 focus:ring-2 ring-indigo-300 shadow m-4 p-4 resize-none"
+        className="textarea textarea-primary font-mono bg-white shadow mx-4 p-4 resize-none w-auto"
         style={{ height: "calc(100vh - 8rem)" }}
         placeholder="[1,2,3]"
       />
-      <div className="text-right mr-4">
-        <Button onClick={prettier}>格式化</Button>
-      </div>
     </div>
   );
 }
